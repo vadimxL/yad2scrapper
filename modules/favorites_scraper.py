34208afc1823a0277c0a365eea236fddf60d7bbf
vadimv <vadim.malinovsky@convexum.com>
2023-07-07 11:03:02 +0300

    add requirements file resolve imports

--- modules/favorites_scraper.py
+++ modules/favorites_scraper.py
@@ -8,11 +8,22 @@ from bs4 import BeautifulSoup
 import glob
 import os
 from datetime import datetime, timedelta
+
+from pymongo import MongoClient
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+def setup_favorites_db():
+    # Connect to MongoDB
+    client = MongoClient("mongodb://localhost:27018")
+    client.drop_database("favorites_yad2")
+    db = client["favorites_yad2"]
+    collection = db["favorites_yad2data"]
+    return collection
 
 
 def save_to_json(out_name, car_listings_sorted):
@@ -85,41 +96,74 @@ def get_html_file_list(new_directory):
     return [str(file_name) for file_name in html_files_list]
 
 
+def get_latest_html_file_without_type(folder_path):
+    # Specify the pattern of the filename
+    filename_pattern = 'scraped_content_*.html'
+
+    # Get a list of all files matching the pattern in the folder
+    html_files = glob.glob(os.path.join(folder_path, filename_pattern))
+
+    # Sort the files by modification time in descending order
+    html_files.sort(key=os.path.getmtime, reverse=True)
+
+    # Get the most recent file
+    most_recent_file = os.path.splitext(os.path.basename(html_files[0]))[0]
+
+    return most_recent_file
+
+
 class Yad2FavoritesScraper:
     # driver = webdriver.Chrome()  # Or use any other driver (e.g., Firefox)
     def __init__(self):
         self.username = "malinvadim88@gmail.com"
         self.password = "Alona1990"
+        self.collection = setup_favorites_db()
+
+        # Create a new directory path next to the Python binary
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        self.html_save_dir = os.path.join(current_directory, "scraped_content")
+        self.csv_save_dir = os.path.join(current_directory, "scraped_content_parsed_csv")
+
+        # Create the new directory if it doesn't exist
+        if not os.path.exists(self.html_save_dir):
+            os.makedirs(self.html_save_dir)
+
+        # Create the new directory if it doesn't exist
+        if not os.path.exists(self.csv_save_dir):
+            os.makedirs(self.csv_save_dir)
+
+    def run(self):
+        file_name = os.path.join(self.html_save_dir,
+                                 f"scraped_content_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.html")
+
+        if not saved_recently(self.html_save_dir):
+            elements = self.get_favorites_car_elements()
+            self.save_list_of_favorite_cars(elements, file_name)
+
+        # Parse HTML files and save the results to CSV files
+        # html_files_list = get_html_file_list(html_save_dir)
+        html_files_list = get_latest_html_file_without_type(self.html_save_dir)
+        for name in html_files_list:
+            file_name = os.path.join(self.html_save_dir, name + '.html')
+            with open(file_name, 'r') as file:
+                html_doc = file.read()
+                car_listings = self.parse_favorites(html_doc)
+                save_to_csv(os.path.join(self.csv_save_dir, name), car_listings)
+                self.save_to_db(car_listings)
+
+    def save_to_db(self, data_for_db):
+        print("Data for db: ", data_for_db)
+        print("Size of data_for_db: ", len(data_for_db))
+        i = 0
+        for item in data_for_db:
+            result = self.collection.insert_one(item)
+            # Print the inserted document's _id
+            print("Inserted document ID:", result.inserted_id)
+            i += 1
+            print("Inserted {} documents".format(i))
 
     def get_mileage(self):
         pass
-
-    # def save_full_info(cls, link):
-    #     cls.driver.get(link)
-    #
-    #     # Wait until the login elements are present
-    #     wait = WebDriverWait(cls.driver, 10)
-    #     cls.driver.get(link)
-    #
-    #     # Wait until the desired class appears
-    #     wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "price")))
-    #
-    #     # Scrape the desired content
-    #     elements = cls.driver.find_elements(By.CLASS_NAME, "price")
-    #
-    #     # Create a file name with the current date and hour
-    #     now = datetime.datetime.now()
-    #     file_name = f"scraped_content_full_{now.strftime('%Y-%m-%d_%H-%M')}.html"
-    #
-    #     # Open the file in write mode
-    #     with open(file_name, "w", encoding="utf-8") as file:
-    #         # Write the HTML content to the file
-    #         file.write("<html><body>")
-    #         for element in elements:
-    #             file.write(element.get_attribute("outerHTML"))
-    #         file.write("</body></html>")
-    #
-    #     print(f"Scraped content saved to {file_name}")
 
     def get_favorites_car_elements(self):
         driver = webdriver.Chrome()  # Or use any other driver (e.g., Firefox)
@@ -162,37 +206,6 @@ class Yad2FavoritesScraper:
             file.write("</body></html>")
 
         print(f"Scraped content saved to {file_name}")
-
-    def run(self):
-        # Create a new directory path next to the Python binary
-        current_directory = os.path.dirname(os.path.abspath(__file__))
-        html_save_dir = os.path.join(current_directory, "scraped_content")
-        csv_save_dir = os.path.join(current_directory, "scraped_content_parsed_csv")
-
-        # Create the new directory if it doesn't exist
-        if not os.path.exists(html_save_dir):
-            os.makedirs(html_save_dir)
-
-        # Create the new directory if it doesn't exist
-        if not os.path.exists(csv_save_dir):
-            os.makedirs(csv_save_dir)
-
-        # Set the path for saving the file in the new directory
-        file_name = os.path.join(html_save_dir,
-                                 f"scraped_content_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.html")
-
-        if not saved_recently(html_save_dir):
-            elements = self.get_favorites_car_elements()
-            self.save_list_of_favorite_cars(elements, file_name)
-
-        # Parse HTML files and save the results to CSV files
-        html_files_list = get_html_file_list(html_save_dir)
-        for name in html_files_list:
-            file_name = os.path.join(html_save_dir, name + '.html')
-            with open(file_name, 'r') as file:
-                html_doc = file.read()
-                car_listings = self.parse_favorites(html_doc)
-                save_to_csv(os.path.join(csv_save_dir, name), car_listings)
 
     def parse_favorites(self, html_doc):
         car_listings = []
@@ -268,26 +281,28 @@ class Yad2FavoritesScraper:
                 seen_ids.add(car_listing["Item ID"])
 
         # Sort the car listings by price in ascending order
-        # car_listings_sorted = sorted(car_listings, key=lambda x: x['Item ID'].replace(',', ''))
-        # car_listings_sorted = car_listing
-
-        # translator = Translator()
-        # # Translate the "Car Make and Model" from Hebrew to English
-        # for listing in car_listings:
-        #     car_make_model_heb = listing['Car Make and Model']
-        #     try:
-        #         car_make_model_eng = translator.translate(car_make_model_heb, dest='en')
-        #     except Exception as e:
-        #         print(f"Translation error: {e}")
-        #         car_make_model_eng = car_make_model_heb
-        #     if type(car_make_model_eng) == str:
-        #         listing['Car Make and Model'] = car_make_model_eng
-        #         print('car_make_model_eng is str', car_make_model_eng)
-        #     else:
-        #         listing['Car Make and Model'] = car_make_model_eng.text
+        # self.translate_car_data(car_listings)
 
         # save_to_json(out_name, car_listings_sorted)
         return car_listings
+
+    def translate_car_data(self, car_listings):
+        car_listings_sorted = sorted(car_listings, key=lambda x: x['Item ID'].replace(',', ''))
+        # translator = Translator()
+        # Translate the "Car Make and Model" from Hebrew to English
+        for listing in car_listings:
+            car_make_model_heb = listing['Car Make and Model']
+            try:
+                pass
+                # car_make_model_eng = translator.translate(car_make_model_heb, dest='en')
+            except Exception as e:
+                print(f"Translation error: {e}")
+                car_make_model_eng = car_make_model_heb
+            if type(car_make_model_eng) == str:
+                listing['Car Make and Model'] = car_make_model_eng
+                print('car_make_model_eng is str', car_make_model_eng)
+            else:
+                listing['Car Make and Model'] = car_make_model_eng.text
 
 
 if __name__ == '__main__':
---
+++ test/test_favorites_scraper.py
@@ -0,0 +1,16 @@
+import unittest
+
+from modules.favorites_scraper import Yad2FavoritesScraper, get_latest_html_file_without_type
+
+
+class MyTestCase(unittest.TestCase):
+    favorites_scraper = Yad2FavoritesScraper()
+
+    def test_load_latest_file_from_dir(self):
+        html_save_dir = self.favorites_scraper.html_save_dir
+        html_files_list = get_latest_html_file_without_type(html_save_dir)
+        print(html_files_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
